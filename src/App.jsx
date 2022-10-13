b5f8101e10d3e685fe272b909ae96cb8092f6c7a
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2022-10-13 15:07:09 +0100

    AS: States with counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import './App.css';
 import Person from './Person';
 import Products from './Components/Props/Products';
 import Parent from './Components/Props/Parent';
+import TrueFalse from './Components/States/TrueFalse';
+import Counter from './Components/States/Counter';
 
 function App() {
 
   const Heading = () => <h1>YO!</h1>
   return (
     <div className="App">
+
+      
         {/* <Heading/>
         <Person/> */}
         {/* {Person()} DODGY */}
 
         {/* PROPS TOPIC: */}
         {/* Props example 1 with products */}
-        <Products/>
-        <Products 
+        {/* <Products/> */}
+        {/* <Products 
         name="Pepsi"
         price={1.50}
         descript="another drink"
@@ -25,9 +29,11 @@ function App() {
         name="Fanta"
         price={3.00}
         descript="again, another drink"
-        />
+        /> */}
         {/* Props example 1 with parent, child & baby */}
-        <Parent/>
+        {/* <Parent/> */}
+        <TrueFalse/>
+        <Counter/>
     </div>
   );
 }
